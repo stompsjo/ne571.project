4bfc37c6946dd89a39c4cc05dc284ea7edce6af4
Jordan Stomps <stomps@wisc.edu>
2021-04-20 10:42:25 -0500

    clearing price duration curves

--- analysis.py
+++ analysis.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+fig, ax = plt.subplots(figsize=[5,4])
+'''
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "sans-serif",
+    "font.sans-serif": ["Helvetica"]})
+'''
+plt.rcParams['font.size'] = 12
 data = np.genfromtxt('data/clearing-price/2019clearing.csv', delimiter=',')
 #print(data[0,1:].reshape((len(data[0,1:]),1)))
 #print(data.shape)
@@ -34,6 +41,7 @@ plt.ylabel('Clearing Price [$/MW]')
 plt.title('7-day Rolling Average Pseudo-Michigan Hub Demand Curve')
 plt.savefig('rolling-ave-demand-curve.png')
 '''
+'''
 lmp = np.genfromtxt('data/lmp/lmp.csv', delimiter=',')
 nodelmp = np.array([])
 for row in lmp[:-1]:
@@ -53,7 +61,45 @@ plt.xlabel('Cumulative Hours at Load')
 plt.ylabel('Hourly Demand [MW]')
 plt.title('WI-MI Load Duration Curve')
 plt.savefig('load-curve.png')
+'''
+'''
+###### Clearing Price Duration Curve for Michigan Hub
+X, F = np.unique(list(reversed(sorted(timeseries))), return_index=True)
+#F = F/float(F.max())
+#ax = plt.gca()
+#ax.invert_xaxis()
+#print(list(reversed(sorted(demand[:,-1]))))
+plt.plot(F,X)
+plt.grid()
+plt.xlim(0,100)
+plt.ylim(0)
+plt.xlabel('Cumulative Hours at Clearing Price')
+plt.ylabel('Clearing Price [$/MW]')
+#plt.title('WI-MI Clearing Price Duration Curve')
+plt.savefig('clearing_duration_curve.png')
+'''
+'''
+lmp = np.genfromtxt('data/lmp/lmp.csv', delimiter=',')
+nodelmp = np.array([])
+for row in lmp[:-1]:
+    #print(row[0])
+    nodelmp = np.append(nodelmp, row[4:])
 
+print(nodelmp.shape)
+X, F = np.unique(list(reversed(sorted(nodelmp))), return_index=True)
+ax.plot(F,X)
+plt.grid()
+#plt.xlim(0,max(F))
+ax.set_xlim(0,max(F))
+ax.set_ylim(0)
+#plt.ylim(0)
+
+#ax.set_xlabel('Cumulative Hours at Clearing Price')
+#ax.set_ylabel('Clearing Price [$/MW]')
+#ax.set_title('Palisades Node Clearing Price Duration Curve')
+plt.tight_layout()
+plt.savefig('node_clearing_duration_curve.png')
+'''
 '''
 print(nodelmp.shape)
 plt.scatter(demand[:,-1],nodelmp)
